cdd0ec11ae4c30bd7f6aaf470029951c7286edc1
Nail <shaykhrazievnail@gmail.com>
2018-12-10 00:26:30 +0300

    Rename val

--- app/src/main/kotlin/com/itis/android/githubapp/ui/auth/LoginViewModel.kt
+++ app/src/main/kotlin/com/itis/android/githubapp/ui/auth/LoginViewModel.kt
@@ -2,22 +2,20 @@ package com.itis.android.githubapp.ui.auth
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.itis.android.githubapp.api.service.AuthService
 import com.itis.android.githubapp.model.common.Outcome
 import com.itis.android.githubapp.repository.AuthRepository
 import com.itis.android.githubapp.repository.PreferenceRepository
-import com.itis.android.githubapp.repository.UserRepository
 import io.reactivex.rxkotlin.subscribeBy
 
 class LoginViewModel(
-        private val authService: AuthRepository,
+        private val authRepository: AuthRepository,
         private val preferenceRepository: PreferenceRepository
 ) : ViewModel() {
 
     private val result = MutableLiveData<Outcome<String>>()
 
     fun auth(login: String, password: String): MutableLiveData<Outcome<String>> {
-        authService.auth(login, password)
+        authRepository.auth(login, password)
                 .map {
                     preferenceRepository.saveAuthToken(it.token)
                     it
