4cf8a8eac8bf2a2a7abced28b0cf4bd686e80059
Nail <shaykhrazievnail@gmail.com>
2019-02-16 20:53:27 +0300

    Update version

--- app/src/test/java/com/itis/android/githubapp/LoginTest.kt
+++ app/src/test/java/com/itis/android/githubapp/LoginTest.kt
@@ -3,10 +3,7 @@ package com.itis.android.githubapp
 import com.google.gson.JsonObject
 import com.itis.android.githubapp.api.service.AuthService
 import com.itis.android.githubapp.model.Authorization
-import com.itis.android.githubapp.repository.AuthRepository
-import com.itis.android.githubapp.repository.PreferenceRepository
 import com.itis.android.githubapp.repository.impl.AuthRepositoryImpl
-import com.itis.android.githubapp.ui.auth.LoginViewModel
 import io.mockk.*
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -15,31 +12,8 @@ import kotlin.test.assertEquals
 
 class LoginTest {
 
-    private val mockAuthRepository: AuthRepository = mockk()
-    private val mockPrefRepository: PreferenceRepository = mockk()
-
-    private val loginViewModel = LoginViewModel(mockAuthRepository, mockPrefRepository)
-
     private val mockAuthService: AuthService = mockk()
     private val authRepository = spyk(AuthRepositoryImpl(mockAuthService))
-
-//    @Test
-//    fun whenLoginExpectedSuccess() = runBlocking(Dispatchers.Main) {
-//        val expectedLogin = "Login"
-//        val expectedPass = "Pass"
-//        val expectedAuth = Authorization("token")
-//
-//        coEvery {
-//            mockAuthRepository.getAuthAsync(expectedLogin, expectedPass).await()
-//        } returns expectedAuth
-//
-//        runBlocking(Dispatchers.Main) {
-//            loginViewModel.auth(expectedLogin, expectedPass)
-//
-////            coVerify { loginViewModel.auth(expectedLogin, expectedPass) }
-////            coVerify { mockPrefRepository.saveAuthToken("token") }
-//        }
-//    }
 
     @Test
     @DisplayName("Но это был я...")
